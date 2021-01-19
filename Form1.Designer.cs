a01c232e17b888952e50542b2917e13d5ff50b5c
UladzislauKulai <77660457+UladzislauKulai@users.noreply.github.com>
2021-01-19 07:58:28 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Tetris
+{
+    partial class Form1
+    {
+       
+        private System.ComponentModel.IContainer components = null;
+
+      
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region
+
+      
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pauseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tryagainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.howtoplayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(632, 30);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pauseToolStripMenuItem,
+            this.tryagainToolStripMenuItem,
+            this.howtoplayToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(60, 26);
+            this.menuToolStripMenuItem.Text = "Menu";
+            this.menuToolStripMenuItem.Click += new System.EventHandler(this.menuToolStripMenuItem_Click);
+            // 
+            // pauseToolStripMenuItem
+            // 
+            this.pauseToolStripMenuItem.Name = "pauseToolStripMenuItem";
+            this.pauseToolStripMenuItem.Size = new System.Drawing.Size(173, 26);
+            this.pauseToolStripMenuItem.Text = "Pause";
+            this.pauseToolStripMenuItem.Click += new System.EventHandler(this.OnPauseButtonClick);
+            // 
+            // tryagainToolStripMenuItem
+            // 
+            this.tryagainToolStripMenuItem.Name = "tryagainToolStripMenuItem";
+            this.tryagainToolStripMenuItem.Size = new System.Drawing.Size(173, 26);
+            this.tryagainToolStripMenuItem.Text = "Try again";
+            this.tryagainToolStripMenuItem.Click += new System.EventHandler(this.OnAgainButtonClick);
+            // 
+            // howtoplayToolStripMenuItem
+            // 
+            this.howtoplayToolStripMenuItem.Name = "howtoplayToolStripMenuItem";
+            this.howtoplayToolStripMenuItem.Size = new System.Drawing.Size(173, 26);
+            this.howtoplayToolStripMenuItem.Text = "How to play";
+            this.howtoplayToolStripMenuItem.Click += new System.EventHandler(this.OnInfoPressed);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Jokerman", 19.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(467, 307);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 49);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Jokerman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(468, 240);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(632, 603);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Jokerman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Tetris";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pauseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tryagainToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem howtoplayToolStripMenuItem;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label label2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tetris.Controllers;
+
+namespace Tetris
+{
+    public partial class Form1 : Form
+    {
+        
+        
+        string name;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+            name = Microsoft.VisualBasic.Interaction.InputBox("Enter player name", "Player name","New player");
+            if(name== "")
+            {
+                name = "New player";
+            }
+            this.KeyUp += new KeyEventHandler(keyFunc);
+            Init();
+        }
+
+        public void Init()
+        { 
+            this.Text = "Current player - " + name;
+    
+            MapController.lines = 0;
+            MapController.score = 0;
+            MapController.currentShape = new Shape(0, 0);
+            MapController.interval = 300;
+            MapController.size = 27;
+            label1.Text = "Score: " + MapController.score;
+            label2.Text = "Lines: " + MapController.lines;
+
+           
+
+            timer1.Interval = MapController.interval;
+            timer1.Tick += new EventHandler(update);
+            timer1.Start();
+            
+
+            Invalidate();
+        }
+
+        private void keyFunc(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+
+            {
+                case Keys.Z:
+
+                    if (!MapController.IsIntersects())
+                    {
+                        MapController.ResetArea();
+                        MapController.currentShape.RotateShape();
+                        MapController.Merge();
+                        Invalidate();
+                    }
+                    break;
+                case Keys.Down:
+                    timer1.Interval = 20;
+                    break;
+                case Keys.Right:
+                    if (!MapController.CollideHor(1))
+                    {
+                        MapController.ResetArea();
+                        MapController.currentShape.MoveRight();
+                        MapController.Merge();
+                        Invalidate();
+                    }
+                    break;
+                case Keys.Left:
+                    if (!MapController.CollideHor(-1))
+                    {
+                        MapController.ResetArea();
+                        MapController.currentShape.MoveLeft();
+                        MapController.Merge();
+                        Invalidate();
+                    }
+                    break;
+            }
+        }
+
+        
+        private void update(object sender, EventArgs e)
+        {
+            MapController.ResetArea();
+            if (!MapController.Collide())
+            {
+                MapController.currentShape.MoveDown();
+            }
+            else
+            {
+                MapController.Merge();
+                MapController.SliceMap(label1,label2);
+                timer1.Interval = MapController.interval;
+                MapController.currentShape.ResetShape(3,0);
+                if (MapController.Collide())
+                {
+                    MapController.ClearMap();
+                    timer1.Tick -= new EventHandler(update);
+                    timer1.Stop();
+                    MessageBox.Show("Result :  " + MapController.score);
+                    Init();
+                }
+            }
+            MapController.Merge();
+            Invalidate();
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            MapController.DrawGrid(e.Graphics);
+            MapController.DrawMap(e.Graphics);
+            MapController.ShowNextShape(e.Graphics);
+        }
+
+        private void OnPauseButtonClick(object sender, EventArgs e)
+        {
+            var pressedButton = sender as ToolStripMenuItem;
+            if (timer1.Enabled)
+            {
+                pressedButton.Text = "Continue";
+                timer1.Stop();
+            }
+            else
+            {
+                pressedButton.Text = "Pause";
+                timer1.Start();
+            }
+        }
+
+        private void OnAgainButtonClick(object sender, EventArgs e)
+        {
+            timer1.Tick -= new EventHandler(update);
+            timer1.Stop();
+            MapController.ClearMap();
+            Init();
+        }
+
+
+        private void OnInfoPressed(object sender, EventArgs e)
+        {
+            string infoString = "";
+            infoString = "To rotate figure use 'Z'.\n";
+            infoString += "Left arrow - Move left.\n";
+            infoString += "Right arrow - Move right.\n";
+            infoString += "Down arrow - Fast drop.\n";
+            MessageBox.Show(infoString,"Help");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+           
+        }
+       
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
